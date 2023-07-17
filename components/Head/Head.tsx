57c377106d56aecead01df4d351ec21a6a6166f8
Gabriel <estudo.gabmartts@gmail.com>
2023-07-17 17:26:53 -0300

    Head head head

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -1,30 +1,39 @@
+import Image from "next/image";
+import paquetaImage from 'public/paquetahead.svg'
+import headphoneSvg from 'public/icons/headphone.svg'
+import placeSvg from "public/icons/place.svg"
+import heartSvg from "public/icons/heart.svg"
+import bagSvg from "public/icons/bag.svg"
+import userSvg from "public/icons/user.svg"
+
+
 export function HeaderComponent (){
     return (
         <header>
-            <div className="bg-orange-400 flex justify-around">
+            <div className="bg-[#FF8A29] p-2 text-white flex justify-around">
                 <span>Loja de Calçados e Moda Online</span>
                 <div className="flex flex-row gap-6">
-                    <span><h1>Fale conosco</h1></span>
-                    <span><h1>Encontrar uma loja fisica</h1></span>
+                    <span className="flex flex-row gap-2"><Image src={headphoneSvg} width={16} height={16} alt="headphone" /><h1>Fale conosco</h1></span>
+                    <span className="flex flex-row gap-2"><Image src={placeSvg} width={16} height={16} alt="place" /><h1>Encontrar uma loja fisica</h1></span>
                 </div>
             </div>
             <div>
-                <div className="flex justify-around h-16 items-center">
-                    <span>Paqueta</span>
+                <div className="flex justify-around p-6 items-center">
+                    <Image src={paquetaImage} width={297} height={86} alt="Paqueta"/>
                     <div className="flex flex-row gap-6">
-                        <span><h1>Lista de desejo</h1></span>
-                        <span><h1>Sacola</h1></span>
-                        <span><h1>Entrar</h1></span>
+                        <span className="flex flex-row gap-2"><Image src={heartSvg} width={16} height={16} alt="heart" /><h1>Lista de desejo</h1></span>
+                        <span className="flex flex-row gap-2"><Image src={bagSvg} width={16} height={16} alt="bag" /><h1>Sacola</h1></span>
+                        <span className="flex flex-row gap-2"><Image src={userSvg} width={16} height={16} alt="login" /><h1>Entrar</h1></span>
 
                     </div>
                 </div>
-                <div className="flex justify-around">
-                    <span>Novidades</span>
-                    <span>Feminino</span>
-                    <span>Masculino</span>
-                    <span>Paquetá Esportes</span>
-                    <span>Outlet</span>
-                    <span>Cashback</span>
+                <div className="flex text-black font-medium text-lg justify-around">
+                    <a>Novidades</a>
+                    <a>Feminino</a>
+                    <a>Masculino</a>
+                    <a>Paquetá Esportes</a>
+                    <a>Outlet</a>
+                    <a>Cashback</a>
                 </div>
             </div>
         </header>

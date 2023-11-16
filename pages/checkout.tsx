d75084b3e3dd5e14458ea0337cc795e1a5101c82
Gabriel <estudo.gabmartts@gmail.com>
2023-11-16 16:21:01 -0300

    feat: checkout

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,18 +1,25 @@
 import Image from "next/image"
 import useCart from "../hooks/useCart"
 import deleteSVG from "../public/icons/delete.svg"
+import Link from "next/link"
 export default function Checkout (){
     const {cart, removeItemFromCart} = useCart()
     return(
-        <main>
-            {cart.length}
-            <h1>Sacola de compras</h1>
+        <main className="m-auto w-4/5 sm:w-3/5 flex flex-col md:p-20">
+            <h1 className="font-bold">Sacola de compras</h1>
+            <div className="m-auto w-full flex flex-col gap-3 items-center p-20 bg-[#f9f9f9]">
+                {cart.length == 0 && (<>
+                    <h1>Aparentemente seu carrinho está vazio</h1>
+                    <Link className="w-56 text-center p-2 rounded-md bg-orange-400 text-white font-bold border-2" href="/">Voltar às compras</Link>
+                </>)}
             {cart.map(e => (
-            <div className="w-96 h-44 border-2">
+            <div className="h-44 w-full border-b-2">
                 <p>Id do produto : {e.productId}</p><p>Quantidade: {e.quantity}</p>
                 <button onClick={() => removeItemFromCart(e.productId)}><span><Image width={25} height={25} src={deleteSVG} alt="" /></span></button>
             </div>
             ))}
+            </div>
+            
         </main>
     )
 }

cc99ccb3b23cb1bc5216e0adeb32d4dd77bd6f26
Gabriel <estudo.gabmartts@gmail.com>
2023-11-26 20:21:49 -0300

    Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -67,7 +67,7 @@ export function Footer () {
             </div>
             <div className="flex justify-center p-3 items-center bg-[#383838] text-white">
                 <span className="uppercase font-semibold">uma empresa do grupo</span>
-                <Image className="fill-white" alt="" src={PaquetaSvg} width={265} height={49} />
+                <Image alt="" src={PaquetaSvg} width={265} height={49} />
             </div>
         </footer>
     )
